dc1b4ca40483841e385d04bf813a5525cf64671a
Matthew Guo <mg2239@cornell.edu>
2020-12-20 21:15:04 -0500

    finished basic full page layout

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,21 +1,28 @@
+import Button from '../../common/Button/Button';
 import { projectData, ProjectType } from './data';
 import styles from './projects.module.css';
 
+const Divider = () => <hr className={styles.divider} />;
+
 const Project = ({ caption, link, linkType, img, title }: ProjectType) => (
-  <div>
-    <img src={img} alt={title} />
-    <div>
-      <h1>{title}</h1>
-      <p>{caption}</p>
-      <a href={link}>{linkType}</a>
+  <div className={styles.ctr}>
+    <img src={img} alt={title} className={styles.img} />
+    <div className={styles.infoCtr}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.caption}>{caption}</p>
+      <Button to={link}>{linkType}</Button>
     </div>
   </div>
 );
 
 export default () => (
   <>
-    {projectData.map((project) => (
-      <Project {...project} />
-    ))}
+    {projectData.reduce((acc, project, index) => {
+      acc.push(<Project {...project} />);
+      if (index < projectData.length - 1) {
+        acc.push(<Divider />);
+      }
+      return acc;
+    }, [] as JSX.Element[])}
   </>
 );
