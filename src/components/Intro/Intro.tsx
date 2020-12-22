6dd4d300ce78bf1ef0d3a8d612faeeac95abbda9
Matthew Guo <mg2239@cornell.edu>
2020-12-21 20:30:16 -0500

    switched image, edited intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,6 +1,6 @@
 import Icons from '../Icons/Icons';
 import styles from './intro.module.css';
-import img from './websiteimg.jpg';
+import img from './img.jpg';
 
 export default () => (
   <div className={styles.ctr}>
@@ -13,11 +13,12 @@ export default () => (
         I’m a junior at Cornell University studying Computer Science.
       </p>
       <p className={styles.text}>
-        Previously interned at{' '}
+        I previously interned at{' '}
         <a href="https://www.linkedin.com/company/bloom-university/about/">
           Bloom
         </a>
-        , incoming SWE intern at <a href="https://www.cameo.com/">Cameo</a>.
+        , and am an incoming SWE intern at{' '}
+        <a href="https://www.cameo.com/">Cameo</a>.
       </p>
       <p className={styles.text}>
         In my free time, I like working on personal projects, producing music,
