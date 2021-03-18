e2e8f583368274b14c04396501fd8175be2c281c
Matthew Guo <mguo22@gmail.com>
2021-03-17 23:27:47 -0400

    Update data.ts

--- src/components/Projects/data.ts
+++ src/components/Projects/data.ts
@@ -12,7 +12,7 @@ export const projectData: ProjectType[] = [
   {
     title: 'Carriage',
     caption:
-      'Ride scheduling and management for CULift. Being created as part of Cornell Design and Tech Initiative, a project team on campus.',
+      'Ride scheduling, tracking, and management for CULift. Being created as part of Cornell Design and Tech Initiative, a project team on campus.',
     img: carriage,
     link: 'https://github.com/cornell-dti/carriage-web',
     linkType: 'GitHub',
